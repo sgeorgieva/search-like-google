25fc19e0d9698610d4fcebd4b3a3d7d2f892cb79
Santiya Georgieva <santiya@abv.bg>
2023-05-18 12:23:28 +0300

    Remove comment lines in App.js

--- src/App.js
+++ src/App.js
@@ -32,10 +32,6 @@ function App() {
   ];
 
   const fetchData = (value) => {
-    // fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCK0WH6eGqTdvrlrxr_tdRZFmaVav-_qvw&cx=c1a00945955654fe9&q=${value}`)
-    // fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCiCEHRd-P9eWRmCIvtkpAXmi5RE2nXwqQ&cx=c1a00945955654fe9&q=${value}`)
-    // fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCiCEHRd-P9eWRmCIvtkpAXmi5RE2nXwqQ&cx=c1a00945955654fe9&queries=nextPage&q=${value}`)
-    // fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAC3Q6ik9U_ykisoj2xwGnSXPKXWButTLI&cx=c1a00945955654fe9&q=${value}`)
     fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyA_Hm6-4jn8yTgM9tw1xJZ5U8scR_t2U40&cx=c1a00945955654fe9&q=${value}`)
       .then(response => response.ok ? response.json() : Promise.reject(response))
       .then(data => {
